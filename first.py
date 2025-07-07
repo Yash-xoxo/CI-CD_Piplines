817b2fe2449775279fa0ec15ac4d55758e5c6362
Yash <satorufxd@gmail.com>
2025-07-07 11:27:21 +0530

    fd

--- first.py
+++ first.py
@@ -4,6 +4,17 @@ def lw_sum(x, y):
 def lw_mul(x, y):
     return x * y
 
+def lw_sub(x, y):
+    return x - y
+
+def lw_div(x, y):
+    if y == 0:
+        return "MATH ERROR"
+    else:
+        return x/y
+    
+    
+
 #@app.route("/info")
 def get_info():
     return "Hello from the APP"
--- test_myapp.py
+++ test_myapp.py
@@ -8,3 +8,13 @@ def test_sum():
 def test_mul():
     assert lw_mul(2, 3) == 6
     assert lw_mul(5, 6) == 30
+
+def test_sub():
+    assert lw_mul(2, 8) == -6
+    assert lw_mul(13, 4) == 9
+
+def test_div():
+    assert lw_mul(9, 3) == 3
+    assert lw_mul(45, 9) == 5
+
+
